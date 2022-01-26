b61bba142bd1355ef2bb716185e46a6946a775ea
AidenFox <miomoor182@gmail.com>
2022-01-26 12:29:03 +0300

    Working modal with content from file! :fire:
    Now all modal applications parse their content from `modal/{mdlName}` :100:

--- webkit/app/js/foxmdl.js
+++ webkit/app/js/foxmdl.js
@@ -1,91 +1,91 @@
 
 var FoxMdl = (function(){
 
-	var container = document.documentElement,
-		popup = document.querySelector( '.foxmdl-popup-animate' ),
-		cover = document.querySelector( '.foxmdl-cover' ),
+	let container = document.documentElement,
+		popup = document.querySelector('.foxmdl-popup-animate'),
+		cover = document.querySelector('.foxmdl-cover'),
 		currentState = null;
 
-	container.classList.add( 'foxmdl-ready' );
+	container.classList.add('foxmdl-ready');
 
 	// Deactivate on ESC
-	function onDocumentKeyUp( event ) {
-
-		if( event.keyCode === 27 ) {
+	function onDocumentKeyUp(event) {
+		if(event.keyCode === 27) {
 			deactivate();
 		}
-
 	}
 
 	// Deactivate on click outside
-	function onDocumentClick( event ) {
-
-		if( event.target === cover ) {
+	function onDocumentClick(event) {
+		if(event.target === cover) {
 			deactivate();
 		}
-
 	}
 
-	function activate( state ) {
+	function activate(state) {
 
-		document.addEventListener( 'keyup', onDocumentKeyUp, false );
-		document.addEventListener( 'click', onDocumentClick, false );
-		document.addEventListener( 'touchstart', onDocumentClick, false );
+		document.addEventListener('keyup', onDocumentKeyUp, false);
+		document.addEventListener('click', onDocumentClick, false);
+		document.addEventListener('touchstart', onDocumentClick, false);
 
-		popup.classList.remove( currentState );
-		popup.classList.add( 'no-transition' );
+		popup.classList.remove(currentState);
+		popup.classList.add('no-transition');
 		if(state)
-			popup.classList.add( state );
+			popup.classList.add(state);
 
-		setTimeout( function() {
-			popup.classList.remove( 'no-transition' );
-			container.classList.add( 'foxmdl-active' );
-		}, 0 );
+		setTimeout(function() {
+			popup.classList.remove('no-transition');
+			container.classList.add('foxmdl-active');
+		}, 0);
 
 		currentState = state;
 
 	}
 
 	function deactivate() {
-
-		document.removeEventListener( 'keyup', onDocumentKeyUp, false );
-		document.removeEventListener( 'click', onDocumentClick, false );
-		document.removeEventListener( 'touchstart', onDocumentClick, false );
-
-		container.classList.remove( 'foxmdl-active' );
-		popup.classList.remove( 'foxmdl-popup-animate' );
+		document.removeEventListener('keyup', onDocumentKeyUp, false);
+		document.removeEventListener('click', onDocumentClick, false);
+		document.removeEventListener('touchstart', onDocumentClick, false);
+		container.classList.remove('foxmdl-active');
+		popup.classList.remove('foxmdl-popup-animate');
 
 	}
 
-	function disableBlur() {
-
-		document.documentElement.classList.add( 'no-blur' );
-
-	}
-
-	function show( selector ) {
-
-		popup = document.querySelector( selector );
-		popup.classList.add( 'foxmdl-popup-animate' );
+	function show(selector) {
+		console.log('Showing ' + selector);
+		$('.loadedPage').append(modalFill(selector.replace('#', "")));
+		popup = document.querySelector(selector);
+		popup.classList.add('foxmdl-popup-animate');
 		activate();
 		return this;
-
+	}
+	
+	function modalFill(mdlName){
+		let modalFile = getHTMLcontent('modal/'+mdlName+'.html');
+		let modalContent = '<aside id="'+mdlName+'" class="foxmdl-popup" tabindex="-1">' +
+			'<i class="fa fa-times closeButt" onclick="FoxMdl.hide();" aria-hidden="true"></i>' +
+			'<main class="foxMdlMain">' +
+				'<content>' +
+				modalFile +
+				'</content>' +
+				'<footer>' +
+					'<!-- <button class="butt" onclick="FoxMdl.hide();">Close</button> -->' +
+				'</footer>' +
+			'</main>' +
+		'</aside>';
+		
+		return modalContent;
 	}
 
 	function hide() {
-
 		deactivate();
-
 	}
 
 	return {
-
 		activate: activate,
 		deactivate: deactivate,
-		disableBlur: disableBlur,
 		show: show,
 		hide: hide
-
 	}
 
 })();
